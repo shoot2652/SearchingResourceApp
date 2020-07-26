45ddbc454e5cdedb13846f6c7c6fb75034174cee
shoot2652 <thefat2652@gmail.com>
2020-07-26 17:36:18 +1000

    Create SpecificResource.py

---
+++ SpecificResource.py
@@ -0,0 +1,244 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Specific resource window.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1208, 866)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.lSearchForResource = QtWidgets.QLabel(self.centralwidget)
+        self.lSearchForResource.setGeometry(QtCore.QRect(280, 190, 131, 16))
+        self.lSearchForResource.setObjectName("lSearchForResource")
+        self.pteSearch = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.pteSearch.setGeometry(QtCore.QRect(280, 210, 681, 31))
+        self.pteSearch.setObjectName("pteSearch")
+        self.btnSearch = QtWidgets.QPushButton(self.centralwidget)
+        self.btnSearch.setGeometry(QtCore.QRect(970, 210, 93, 28))
+        self.btnSearch.setObjectName("btnSearch")
+        self.btnAddResource = QtWidgets.QPushButton(self.centralwidget)
+        self.btnAddResource.setGeometry(QtCore.QRect(870, 250, 91, 31))
+        self.btnAddResource.setObjectName("btnAddResource")
+        self.lCategories = QtWidgets.QLabel(self.centralwidget)
+        self.lCategories.setGeometry(QtCore.QRect(20, 270, 131, 41))
+        font = QtGui.QFont()
+        font.setPointSize(13)
+        self.lCategories.setFont(font)
+        self.lCategories.setObjectName("lCategories")
+        self.btnCat1 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat1.setGeometry(QtCore.QRect(10, 310, 191, 28))
+        self.btnCat1.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat1.setObjectName("btnCat1")
+        self.btnCat2 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat2.setGeometry(QtCore.QRect(10, 340, 191, 28))
+        self.btnCat2.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat2.setObjectName("btnCat2")
+        self.btnCat3 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat3.setGeometry(QtCore.QRect(10, 370, 191, 28))
+        self.btnCat3.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat3.setObjectName("btnCat3")
+        self.btnCat4 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat4.setGeometry(QtCore.QRect(10, 400, 191, 28))
+        self.btnCat4.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat4.setObjectName("btnCat4")
+        self.btnCat7 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat7.setGeometry(QtCore.QRect(10, 490, 191, 28))
+        self.btnCat7.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat7.setObjectName("btnCat7")
+        self.btnCat5 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat5.setGeometry(QtCore.QRect(10, 430, 191, 28))
+        self.btnCat5.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat5.setObjectName("btnCat5")
+        self.btnCat8 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat8.setGeometry(QtCore.QRect(10, 520, 191, 28))
+        self.btnCat8.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat8.setObjectName("btnCat8")
+        self.btnCat6 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat6.setGeometry(QtCore.QRect(10, 460, 191, 28))
+        self.btnCat6.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat6.setObjectName("btnCat6")
+        self.btnCat11 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat11.setGeometry(QtCore.QRect(10, 610, 191, 28))
+        self.btnCat11.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat11.setObjectName("btnCat11")
+        self.btnCat13 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat13.setGeometry(QtCore.QRect(10, 670, 191, 28))
+        self.btnCat13.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat13.setObjectName("btnCat13")
+        self.btnCat9 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat9.setGeometry(QtCore.QRect(10, 550, 191, 28))
+        self.btnCat9.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat9.setObjectName("btnCat9")
+        self.btnCat14 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat14.setGeometry(QtCore.QRect(10, 700, 191, 28))
+        self.btnCat14.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat14.setObjectName("btnCat14")
+        self.btnCat12 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat12.setGeometry(QtCore.QRect(10, 640, 191, 28))
+        self.btnCat12.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat12.setObjectName("btnCat12")
+        self.btnCat16 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat16.setGeometry(QtCore.QRect(10, 760, 191, 28))
+        self.btnCat16.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat16.setObjectName("btnCat16")
+        self.btnCat15 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat15.setGeometry(QtCore.QRect(10, 730, 191, 28))
+        self.btnCat15.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat15.setObjectName("btnCat15")
+        self.btnCat10 = QtWidgets.QPushButton(self.centralwidget)
+        self.btnCat10.setGeometry(QtCore.QRect(10, 580, 191, 28))
+        self.btnCat10.setStyleSheet("background-color: transparent;\n"
+"border-image: url(:off.png);\n"
+"background: none;\n"
+"border: none;\n"
+"background-repeat: none;")
+        self.btnCat10.setObjectName("btnCat10")
+        self.lLogo = QtWidgets.QLabel(self.centralwidget)
+        self.lLogo.setGeometry(QtCore.QRect(0, 0, 1280, 261))
+        self.lLogo.setText("")
+        self.lLogo.setPixmap(QtGui.QPixmap("Resources/Thomastown secondary college bannar.png"))
+        self.lLogo.setObjectName("lLogo")
+        self.lBackground = QtWidgets.QLabel(self.centralwidget)
+        self.lBackground.setGeometry(QtCore.QRect(0, 240, 1201, 621))
+        self.lBackground.setText("")
+        self.lBackground.setPixmap(QtGui.QPixmap("Resources/plain-white-background.jpg"))
+        self.lBackground.setObjectName("lBackground")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(760, 360, 311, 451))
+        self.label.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.label.setObjectName("label")
+        self.lBackground.raise_()
+        self.lLogo.raise_()
+        self.lSearchForResource.raise_()
+        self.pteSearch.raise_()
+        self.btnSearch.raise_()
+        self.btnAddResource.raise_()
+        self.lCategories.raise_()
+        self.btnCat1.raise_()
+        self.btnCat2.raise_()
+        self.btnCat3.raise_()
+        self.btnCat4.raise_()
+        self.btnCat7.raise_()
+        self.btnCat5.raise_()
+        self.btnCat8.raise_()
+        self.btnCat6.raise_()
+        self.btnCat11.raise_()
+        self.btnCat13.raise_()
+        self.btnCat9.raise_()
+        self.btnCat14.raise_()
+        self.btnCat12.raise_()
+        self.btnCat16.raise_()
+        self.btnCat15.raise_()
+        self.btnCat10.raise_()
+        self.label.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.lSearchForResource.setText(_translate("MainWindow", "Search for resource..."))
+        self.btnSearch.setText(_translate("MainWindow", "Search"))
+        self.btnAddResource.setText(_translate("MainWindow", "Add Resource"))
+        self.lCategories.setText(_translate("MainWindow", "Categories "))
+        self.btnCat1.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat2.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat3.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat4.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat7.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat5.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat8.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat6.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat11.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat13.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat9.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat14.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat12.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat16.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat15.setText(_translate("MainWindow", "PushButton"))
+        self.btnCat10.setText(_translate("MainWindow", "PushButton"))
+        self.label.setText(_translate("MainWindow", "change"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
